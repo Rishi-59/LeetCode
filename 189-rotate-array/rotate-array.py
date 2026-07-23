be664e2b177695c88d026bd1559922431adeaaef
Kunal More <145890883+Rishi-59@users.noreply.github.com>
2026-07-24 01:06:39 +0530

    Time: 7 ms (53.57%) | Memory: 27.1 MB (7.87%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,21 +4,28 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # def rotate1():
-           
         #     temp = nums[-1]
-
         #     for i in range(len(nums)-1,0,-1):
         #         nums[i] = nums[i-1]
-
         #     nums[0] = temp
-
         # for _ in range(k):
         #     rotate1()
+
+
+        # k %= len(nums) 
+        # nums[:] = nums[-k:] + nums[:-k]
+        def reverse(l,r):
+            while l<r:
+                temp = nums[l]
+                nums[l] = nums[r]
+                nums[r] = temp 
+                l += 1
+                r -= 1
         k %= len(nums) 
-        if len(nums) < 2 or k==0:
-            return nums
-        fwd = nums[-k:]
-        last = nums[:len(nums)-k]
-        nums[:] = fwd + last
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k,len(nums)-1)
+
+        
 
         
