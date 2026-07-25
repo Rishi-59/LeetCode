362586cca04829edda1c8b11d514778b05171310
Kunal More <145890883+Rishi-59@users.noreply.github.com>
2026-07-26 01:23:39 +0530

    Time: 35 ms (91.17%) | Memory: 42.9 MB (73.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,19 +1,37 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        p = []
-        n = []
+        
+        ###############################
+        # p = []
+        # n = []
+
+        # for i in nums:
+        #     if i < 0:
+        #         n.append(i)
+        #     else:
+        #         p.append(i)
+
+        # nums = []
+        # for i in range(len(n)):
+        #     nums.append(p[i])
+        #     nums.append(n[i])
+        
+        # return nums
+        
+        ###############################
+        n = len(nums)
+        ans = [0] * n
+        p = 0
+        n = 1
 
         for i in nums:
-            if i < 0:
-                n.append(i)
+            if i > 0:
+                ans[p] = i
+                p += 2
             else:
-                p.append(i)
-
-        nums = []
-        for i in range(len(n)):
-            nums.append(p[i])
-            nums.append(n[i])
+                ans[n] = i
+                n += 2
         
-        return nums
-        
-
+        return ans
+            
+                
