924dafc315e3cae34e7db1ab47b39622aa962e7c
Kunal More <145890883+Rishi-59@users.noreply.github.com>
2026-07-31 10:51:00 +0530

    Time: 3 ms (76.79%) | Memory: 19 MB (98.64%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,13 +1,29 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        # roman = {
+        #     'M': 1000,
+        #     'D': 500,
+        #     'C': 100,
+        #     'L': 50,
+        #     'X': 10,
+        #     'V': 5,
+        #     'I': 1
+        # }
+
         roman = {
-            'M': 1000,
-            'D': 500,
-            'C': 100,
-            'L': 50,
-            'X': 10,
-            'V': 5,
-            'I': 1
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
+            "V": 5,
+            "IV": 4,
+            "I": 1
         }
 
         s = ''
@@ -17,14 +33,11 @@ class Solution:
                 s += S
                 curr -= V
 
-        s = s.replace('IIII','IV')
-        s = s.replace('VIV','IX')
-        s = s.replace('XXXX','XL')
-        s = s.replace('LXL','XC')
-        s = s.replace('CCCC','CD')
-        s = s.replace('DCD','CM')
+        # s = s.replace('IIII','IV')
+        # s = s.replace('VIV','IX')
+        # s = s.replace('XXXX','XL')
+        # s = s.replace('LXL','XC')
+        # s = s.replace('CCCC','CD')
+        # s = s.replace('DCD','CM')
         
-        
-        
-
         return s
