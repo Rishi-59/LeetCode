2cd75979b3545edae03893634f3b46cf45b71a90
Kunal More <145890883+Rishi-59@users.noreply.github.com>
2026-07-31 10:28:25 +0530

    Time: 7 ms (38.06%) | Memory: 19.4 MB (6.18%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,30 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+
+        s = ''
+        curr = num
+        for S , V in roman.items():
+            while curr >= V :
+                s += S
+                curr -= V
+
+        s = s.replace('IIII','IV')
+        s = s.replace('VIV','IX')
+        s = s.replace('XXXX','XL')
+        s = s.replace('LXL','XC')
+        s = s.replace('CCCC','CD')
+        s = s.replace('DCD','CM')
+        
+        
+        
+
+        return s
