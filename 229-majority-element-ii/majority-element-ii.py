78c0879043f637496b2a5545708eb81975610ba6
Kunal More <145890883+Rishi-59@users.noreply.github.com>
2026-08-05 15:25:08 +0530

    Time: 8 ms (48.63%) | Memory: 22.3 MB (81.07%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,13 +1,46 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        ctr1 = 0
+        ctr2 = 0
         n = len(nums)
+        el1 = el2 = float('inf')
+        for num in nums:
+            if ctr1 == 0 and el2 != num:
+                ctr1 += 1
+                el1 = num
+            elif ctr2 == 0 and el1 != num:
+                ctr2 += 1
+                el2 = num
+            elif el1 == num:
+                ctr1 += 1
+            elif el2 == num:
+                ctr2 += 1
+            else:
+                ctr1 -= 1
+                ctr2 -= 1
+        
         mm = n / 3
-        hm = {}
         ls = []
-
+        ctr1 = ctr2 = 0
         for i in range(n):
-            hm[nums[i]] = hm.get(nums[i],0) + 1
-            if hm[nums[i]] > mm and nums[i] not in ls:
-                ls.append(nums[i])
+            if nums[i] == el1:
+                ctr1 += 1
+            elif nums[i] == el2:
+                ctr2 += 1
+        if ctr1 > mm:
+            ls.append(el1)
+        if ctr2 > mm:
+            ls.append(el2)
+        
+        return sorted(ls)
+        # n = len(nums)
+        # mm = n / 3
+        # hm = {}
+        # ls = []
 
-        return ls
+        # for i in range(n):
+        #     hm[nums[i]] = hm.get(nums[i],0) + 1
+        #     if hm[nums[i]] > mm and nums[i] not in ls:
+        #         ls.append(nums[i])
+
+        # return ls
